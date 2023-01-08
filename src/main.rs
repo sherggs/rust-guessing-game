d216b8c5cba0a806db04f5df1fcda12cb447b928
Ademola <sherggs@gmail.com>
2023-01-08 06:28:02 +0000

    guessing with comparison

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -7,7 +8,6 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Your secret number is: {secret_number}");
 
-
     println!("Please input your guess number ");
 
     let mut guess = String::new();
@@ -15,5 +15,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read input");
 
+    let guess: u32 = guess.trim().parse().expect("Failed to parse, Please enter a Number");
+
     println!("Your guess is: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("This guess is less than the secret number"),
+        Ordering::Equal => println!("This guess is equal to the secret number"),
+        Ordering::Greater => println!("This guess is greater than the secret number"),
+    }
 }
